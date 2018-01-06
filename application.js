de8e651c6f5206cfccf5b2253c8133c7a204cb33
avmelnyk <melnyk.andreew@gmail.com>
2018-01-07 00:15:02 +0200

    fetch data from data.json

--- application.js
+++ application.js
@@ -1,16 +1,30 @@
 var PhotoTicker= (function () {
   return {
-    showImages: function () {
+  	getImages: function() {
+  			var xhr = new XMLHttpRequest();
+  			xhr.open('GET','data.json', false);
+        xhr.setRequestHeader("Content-Type", "application/json");
+  			xhr.send();
+  			if (xhr.status != 200) { 
+  				alert( xhr.status + ': ' + 'Alarm'); 
+			  } else {
+          var images = JSON.parse(xhr.responseText);
+          return images;
+			  }
+  	},
+
+    showImages: function (images) {
     	images.forEach(function(image_src){
-	 	var div = document.createElement("DIV");
-	 	div.className = "image_container";
-	 	var image = new Image();
-	 	div.appendChild(image);
-		image.src = image_src;
-		document.body.appendChild(div);
+    	 	var div = document.createElement("DIV");
+    	 	div.className = "image_container";
+    	 	var image = new Image();
+    	 	div.appendChild(image);
+    		image.src = image_src;
+    		document.body.appendChild(div);
  });
     },
   };
 })();
 
-PhotoTicker.showImages();
+var images = PhotoTicker.getImages();
+PhotoTicker.showImages(images);
