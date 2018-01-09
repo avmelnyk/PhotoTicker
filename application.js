2b57ade995d3dc57f70a65b338291a9fe962a808
avmelnyk <melnyk.andreew@gmail.com>
2018-01-09 17:45:30 +0200

    split data.json to vk.json and twitter.json

--- application.js
+++ application.js
@@ -1,34 +1,38 @@
-var PhotoTicker= (function () {
-  return {
-  	getImages: function( callback) {
-  			var xhr = new XMLHttpRequest();
-  			var images = new Array();
-  			xhr.open('GET','data.json', true);
-        	xhr.setRequestHeader("Content-Type", "application/json");
-  			xhr.send();
-  			xhr.onreadystatechange = function() { // (3)
-				  if (xhr.readyState != 4) return;
-				  if (xhr.status != 200) {
-				    alert(xhr.status + ': ' + xhr.statusText);
-				  } else {
-				    images = JSON.parse(xhr.responseText);
-		          	return callback(images);
-				  }
+var vk_url = 'vk.json';
+var twitter_url = 'twitter.json';
 
-			}
-  	},
+var PhotoTicker = (function() {
+    return {
+        getImages: function(url, callback) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.onreadystatechange = function() {
+                var images;
+                if (xhr.readyState != 4) return;
+                if (xhr.status != 200) {
+                    alert(xhr.status + ': ' + xhr.statusText);
+                } else {
+                    images = JSON.parse(xhr.responseText);
+                    callback(images);
+                }
+            };
+            xhr.send();
+        },
 
-    showImages: function (images) {
-    	images.forEach(function(image_src){
-    	 	var div = document.createElement("DIV");
-    	 	div.className = "image_container";
-    	 	var image = new Image();
-    	 	div.appendChild(image);
-    		image.src = image_src;
-    		document.body.appendChild(div);
- });
-    },
-  };
+        showImages: function(images) {
+            images.forEach(function(image_src) {
+                var div = document.createElement("DIV");
+                div.className = "image_container";
+                var image = new Image();
+                image.onload = function() {
+                    div.appendChild(image);
+                };
+                image.src = image_src;
+                document.body.appendChild(div);
+            });
+        }
+    };
 })();
 
-var images = PhotoTicker.getImages(PhotoTicker.showImages);
+PhotoTicker.getImages(twitter_url, PhotoTicker.showImages);
+PhotoTicker.getImages(vk_url, PhotoTicker.showImages);
